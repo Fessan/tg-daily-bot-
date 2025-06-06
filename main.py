8986bf6b2b042cfd1b65ae984540edc1516e7003
Fessan <fessan@ya.ru>
2025-06-06 11:25:28 +0700

    add db

--- main.py
+++ main.py
@@ -4,6 +4,48 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message
 from aiogram.filters import Command
 from dotenv import load_dotenv
+import aiosqlite
+
+DB_PATH = "bot.db"  # Имя файла базы данных
+
+async def init_db():
+    async with aiosqlite.connect(DB_PATH) as db:
+        # Таблица чатов
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS chats (
+            chat_id INTEGER PRIMARY KEY,
+            chat_title TEXT,
+            daily_time TEXT
+        );
+        """)
+
+        # Таблица участников
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS participants (
+            chat_id INTEGER,
+            user_id INTEGER,
+            username TEXT,
+            active BOOLEAN DEFAULT TRUE,
+            PRIMARY KEY (chat_id, user_id)
+        );
+        """)
+
+        # Таблица дэйликов
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS daily_reports (
+            chat_id INTEGER,
+            user_id INTEGER,
+            date TEXT,
+            reply_to_message_id INTEGER,
+            message_id INTEGER,
+            text TEXT,
+            created_at TEXT,
+            PRIMARY KEY (chat_id, user_id, date)
+        );
+        """)
+
+        await db.commit()  # Применяем все изменения
+
 
 # Загружаем переменные окружения из .env
 load_dotenv()
